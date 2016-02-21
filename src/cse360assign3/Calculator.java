de7548d199d851b7befe9987f9ee3cf3102858dc
Terry Wen <wen.terry.e@gmail.com>
2016-02-21 15:02:24 -0700

    initial commit

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,76 @@
+package cse360assign3;
+
+/** 
+ * Class to perform basic arithmetic operations on integers.
+ * 
+ * @author Terry Wen PIN 903 CSE360
+ *
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Initializes calculator object, with total starting at zero
+	 * 
+	 * @param none
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal - Returns the current value in the calculator
+	 * 
+	 * @return current total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add - Adds a value to the total
+	 * 
+	 * @param value number to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract - Subtracts a value from the total
+	 * 
+	 * @param value number to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply - Multiplies total by input
+	 * 
+	 * @param value number to be multiplied
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide - Divides total by input
+	 * 
+	 * @param value number to be divided by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * getHistory - returns string of operations performed
+	 * 
+	 * @return
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
